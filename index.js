2369e45090370cddd91dc3a7e817c36f5b2ca265
Eric Juvet <ejuvet@hollandamerica.com>
2015-02-04 16:01:08 -0800

    Initial commit

--- index.js
+++ index.js
@@ -1,47 +1,78 @@
-module.exports = steelToe;
+/*
+ * steelToe/index.js
+ *
+ * Angularized version of https://github.com/jclem/steeltoe
+ * "Don't shoot yourself in the foot while traversing Javascript objects."
+ *
+ * Deeply sets nested properties which may be undefined,
+ * or gets deeply nested object properties
+ *
+ * Usage:
+ *
+ * steelToe.do(someObject).set("a.b.c","somevalue")
+ * steelToe.do(someObject).get("a.b.c") => "somevalue"
+ */
 
-function steelToe (object) {
-  function _steelToe (property) {
-    if (object && property) {
-      return steelToe(object[property]);
-    } else {
-      return property ? steelToe() : object;
-    }
-  }
+angular.module('vendor.steelToe', [])
 
-  _steelToe.set = function (traversalChain, value) {
-    var keys = traversalChain.split('.'),
-        object = _steelToe;
+.service('steelToe', function() {
 
-    for (var i = 0; i < keys.length; i ++) {
-      if (!object()[keys[i]]) {
-        object()[keys[i]] = {};
-      }
+        function steelToe (object) {
 
-      if (i == keys.length - 1) {
-        object()[keys[i]] = value;
-      }
+            function _steelToe (property) {
+                if (object && property) {
+                    return steelToe(object[property]);
+                } else {
+                    return property ? steelToe() : object;
+                }
+            }
 
-      object = object(keys[i]);
-    }
+            _steelToe.set = function (traversalChain, value) {
+                var keys = traversalChain.split('.'),
+                    object = _steelToe;
 
-    return value;
-  }
+                for (var i = 0; i < keys.length; i ++) {
+                    if (!object()[keys[i]]) {
+                        object()[keys[i]] = {};
+                    }
 
-  _steelToe.get = function (traversalChain) {
-    if (traversalChain) {
-      var keys = traversalChain.split('.'),
-          returnObject = _steelToe, i;
+                    if (i == keys.length - 1) {
+                        object()[keys[i]] = value;
+                    }
 
-      for (i = 0; i < keys.length; i += 1) {
-        returnObject = returnObject(keys[i])
-      }
+                    object = object(keys[i]);
+                }
 
-      return returnObject();
-    } else {
-      return _steelToe();
-    }
-  };
+                return value;
+            };
 
-  return _steelToe;
-};
+            _steelToe.get = function (traversalChain) {
+                if (traversalChain) {
+                    var keys = traversalChain.split('.'),
+                        returnObject = _steelToe, i;
+
+                    for (i = 0; i < keys.length; i += 1) {
+                        returnObject = returnObject(keys[i])
+                    }
+
+                    return returnObject();
+                } else {
+                    return _steelToe();
+                }
+            };
+
+            return _steelToe;
+        }
+
+        return {
+
+            "do" : function(object) {
+                return steelToe(object);
+            }
+
+        }
+
+});
+
+
+
